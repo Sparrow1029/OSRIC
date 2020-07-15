2fa28803dc358be5402adb8293936fffc8d888fe
Alex Ray <user.name>
2020-07-15 06:46:03 -0600

    moved and backed up graphql app

--- graphql/character/resolvers.py
+++
@@ -1,5 +0,0 @@
-def resolve_all_characters(query, info):
-    return list(query.objects.all())
-
-def resolve_query(query, info, name):
-    return query.objects.get(name=name)
--- graphql/player/types.py
+++
@@ -1,7 +0,0 @@
-from graphene_mongo import MongoengineObjectType
-from .models import Player as PlayerModel
-
-
-class Player(MongoengineObjectType):
-    class Meta:
-        model = PlayerModel
